f7681f4129e35cf2d86f5b466d42278a6e4b7f07
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-07-24 01:59:15 -0400

    feat: implement yagni with shopping cart example

---
+++ Yagni/ShoppingCartWithYagni.cs
@@ -0,0 +1,26 @@
+﻿namespace Yagni;
+
+public class ShoppingCartWithYagni
+{
+    private List<Item> items;
+    public ShoppingCartWithYagni()
+    {
+        items = [];
+    }
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+    }
+
+    public decimal GetTotal()
+    {
+        // Only required functionality
+        decimal total = 0;
+        foreach (var item in items)
+        {
+            total += item.Price * item.Quantity;
+        }
+
+        return total;
+    }
+}
