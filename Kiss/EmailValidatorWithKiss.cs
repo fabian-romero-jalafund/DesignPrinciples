8a29afc46eb227b7d86f2921fa327299ca782c5e
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-07-24 02:17:43 -0400

    feat: implement kiss in email validator example

---
+++ Kiss/EmailValidatorWithKiss.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kiss;
+
+public class EmailValidatorWithKiss
+{
+    private Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+    public bool Validate(string email)
+    {
+        if (string.IsNullOrEmpty(email))
+        {
+            throw new ArgumentException("Email cannot be null or empty");
+        }
+
+        Match match = regex.Match(email);
+        return match.Success;
+    }
+}
---
+++ Kiss/EmailValidatorWithoutKiss.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kiss;
+
+public class EmailValidatorWithoutKiss
+{
+    private Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+    public bool Validate(string email)
+    {
+        if (string.IsNullOrEmpty(email))
+        {
+            throw new ArgumentException("Email cannot be null or empty");
+        }
+
+        if (!HasAtSymbol(email))
+        {
+            return false;
+        }
+
+        if (!HasDotSymbol(email))
+        {
+            return false;
+        }
+
+        Match match = regex.Match(email);
+        return match.Success;
+    }
+
+    private bool HasAtSymbol(string email)
+    {
+        return email.Contains('@');
+    }
+
+    private bool HasDotSymbol(string email)
+    {
+        return email.Contains('.');
+    }
+}
